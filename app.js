fd321c69af6741c22fbf9e476bd05ae6afdeeb22
at1902 <atishay083@gmail.com>
2022-08-06 17:40:50 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ =require("lodash");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require("passport-facebook");
+const findOrCreate = require("mongoose-findorcreate");
 
 
 const app = express();
@@ -13,7 +19,67 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb+srv://admin-Atishay:Aaarjain@cluster0.sgdkm.mongodb.net/todolistDB");
+// mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const userSchema= new mongoose.Schema({
+  email: String,
+  password: String,
+  googleId: String,
+  secret: String,
+  facebookId: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User= new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/list",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/list"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 const itemsSchema= {
   name: String
@@ -45,27 +111,76 @@ const List=mongoose.model("List", listSchema);
 
 app.get("/", function(req, res) {
 
-  Item.find({}, function(err, foundItems){
+  res.render("home");
+});
 
-    if(foundItems.length===0){
-      Item.insertMany(defaultItems, function(err){
-        if(err){
-          console.log(err);
-        }
-        else{
-          console.log("Successfully inserted default items.");
-        }
-      });
-      res.redirect("/");
-    }
-    else{
-      res.render("list", {listTitle: "Today", newListItems: foundItems});
-    }
+app.get("/auth/google", passport.authenticate('google', { scope: ["profile"] }));
 
+app.get("/auth/google/list",
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    Item.find({}, function(err, foundItems){
+
+      if(foundItems.length===0){
+        Item.insertMany(defaultItems, function(err){
+          if(err){
+            console.log(err);
+          }
+          else{
+            console.log("Successfully inserted default items.");
+          }
+        });
+        res.redirect("/list");
+      }
+      else{
+        res.render("list", {listTitle: "Today", newListItems: foundItems});
+      }
+
+    })
+
+  });
+
+  app.get("/auth/facebook",
+  passport.authenticate("facebook"));
+
+app.get("/auth/facebook/list",
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    Item.find({}, function(err, foundItems){
+
+      if(foundItems.length===0){
+        Item.insertMany(defaultItems, function(err){
+          if(err){
+            console.log(err);
+          }
+          else{
+            console.log("Successfully inserted default items.");
+          }
+        });
+        res.redirect("/");
+      }
+      else{
+        res.render("list", {listTitle: "Today", newListItems: foundItems});
+      }
+
+    })
+  });
+
+
+  app.get("/login",function(req,res){
+    res.render("login");
   })
 
+  app.get("/register",function(req,res){
+    res.render("register");
+  })
 
-});
+  app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+  })
 
 app.post("/", function(req, res){
 
@@ -93,6 +208,40 @@ app.post("/", function(req, res){
   }
 
 });
+
+app.post("/register", function(req,res){
+
+  User.register({username: req.body.username}, req.body.password, function(err,user){
+    if(err){
+      console.log(err);
+      res.redirect("/list");
+    }
+    else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/list");
+      });
+    }
+  });
+});
+
+app.post("/login", function(req,res){
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user,function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/list");
+      })
+    }
+  })
+})
 
 app.post("/delete", function(req,res){
   const checkedItemId=req.body.checkbox;
